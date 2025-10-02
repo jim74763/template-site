44656c770fdd45d0f3399764cfcb1e8565c300f3
jim74763 <164794563+jim74763@users.noreply.github.com>
2025-10-02 13:07:10 +0200

    fixed the sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,63 +1,65 @@
 import { MetadataRoute } from 'next'
- 
+
+const baseUrl = 'https://jimvd.xyz'
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://jimvd.xyz',
+      url: baseUrl,
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://jimvd.xyz/artisan-bakery',
+      url: `${baseUrl}/artisan-bakery`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/whole-foods',
+      url: `${baseUrl}/whole-foods`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/whole-foods/about',
+      url: `${baseUrl}/whole-foods/about`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/whole-foods/products',
+      url: `${baseUrl}/whole-foods/products`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/construction-pro',
+      url: `${baseUrl}/construction-pro`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/construction-pro/contact',
+      url: `${baseUrl}/construction-pro/contact`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/construction-pro/projects',
+      url: `${baseUrl}/construction-pro/projects`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/dental-care',
+      url: `${baseUrl}/dental-care`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://jimvd.xyz/organic-market',
+      url: `${baseUrl}/organic-market`,
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
